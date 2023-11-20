de6dc6d95527c34af0d677179f218d74f34fb159
Skanda Prasad <skandasprasad@outlook.com>
2023-11-20 17:49:34 +0530

    Update enums.py

--- utils/enums.py
+++ utils/enums.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3.11
+
 class NodeType:
     directory = 2
     file = 1
+
     @staticmethod
     def description(stat):
         node = ''
@@ -11,12 +13,31 @@ class NodeType:
             node = 'file'
         return node
 
+class ResultStatus:
+    success = 200
+    failure = 500
+    already_exists = 409
+    not_found = 404
+
+    @staticmethod
+    def description(stat):
+        message = ''
+        if stat == 409:
+            message = 'Error 409 - Item Already Exists!'
+        elif stat == 404:
+            message = 'Error 404 - Item Not Found!'
+        elif stat == 200:
+            message = 'Status 200 - Success.'
+        elif stat == ResultStatus.failure:
+            message = 'Error 500 - Internal failure.'
+        return message
+
 class Status:
     ok = 200
     error = 500
     already_exists = 409
     not_found = 404
-    
+
     @staticmethod
     def description(stat):
         message = ''
@@ -29,3 +50,64 @@ class Status:
         elif stat == Status.error:
             message = 'Error 500 - Internal error.'
         return message
+
+def distribute_file(file_path, nodes):
+    # Split the file into chunks
+    file_chunks = split_file(file_path)
+
+    # Distribute the chunks to different nodes
+    for chunk in file_chunks:
+        node_index = hash(chunk) % len(nodes)
+        node = nodes[node_index]
+        node.store_file_chunk(chunk)
+
+def split_file(file_path):
+    # Read the file contents
+    with open(file_path, 'rb') as f:
+        file_data = f.read()
+
+    # Divide the file data into chunks of equal size
+    chunk_size = 1024 * 1024  # 1 MB
+    chunks = []
+    for i in range(0, len(file_data), chunk_size):
+        chunk = file_data[i:i+chunk_size]
+        chunks.append(chunk)
+
+    return chunks
+
+def store_file_chunk(chunk):
+    # Simulate storing the file chunk in the node's storage
+    print("Storing file chunk: {}".format(chunk))
+    
+# Dead code related to distributed file system
+def retrieve_file(file_path, nodes):
+    # Check if the file exists in any of the nodes
+    file_exists = False
+    for node in nodes:
+        if node.has_file(file_path):
+            file_exists = True
+            break
+
+    if not file_exists:
+        raise FileNotFoundError('File {} does not exist on any nodes'.format(file_path))
+
+    # Download the file chunks from the nodes
+    file_chunks = []
+    for node in nodes:
+        if node.has_file(file_path):
+            node_chunks = node.download_file_chunks(file_path)
+            file_chunks.extend(node_chunks)
+
+    # Assemble the file chunks
+    file_data = b''
+    for chunk in file_chunks:
+        file_data += chunk
+
+    # Write the file data to the local file system
+    with open(file_path, 'wb') as f:
+        f.write(file_data)
+
+def has_file(file_path):
+    # Check if the file exists in the node's storage
+    # ...
+    return False
